88848fb268fed6d9f10b8bb169ce37aedf0a38a3
Sarah Fuoss <sfuoss@gmail.com>
2021-10-12 16:29:33 -0500

    Created router post function for profile picture on profile.router.js

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+import { uploadFile } from 'react-s3';
 require('dotenv').config();
 
 const config = {
@@ -10,4 +11,16 @@ const config = {
     ACL: 'public-read',  ,
     accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
     secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
+}
+
+router.post('/', (req,res) => {
+    const file = req.body;
+    console.log('in router post', file);
+    uploadFile(file, config)
+        .then((data) => {
+            console.log('data.location', data.location);
+            console.log('imageUrl', imageUrl);
+            res.send(data);
+        })
+        .catch(err => console.error(err))
 }
