fcb3a980f0057cd73ad804ca23ae5550af71e429
Sarah Fuoss <sfuoss@gmail.com>
2021-10-12 19:40:38 -0500

    Added body of ProfilePage to allow for uploading a picture and dispatching to the saga.

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-import { uploadFile } from 'react-s3';
 require('dotenv').config();
+import { uploadFile } from 'react-s3';
 
 const config = {
-    bucket: `${process.env.AWS_S3_BUCKET}`,                           // required
-    region: `${process.env.AWS_S3_REGION}`,                           // optional
-    headers: {'Access-Control-Allow-Origin': '*'},  		    // optional
-    ACL: 'public-read',  ,
+    bucketName: `${process.env.AWS_S3_BUCKET}`,
+    region: `${process.env.AWS_S3_REGION}`,
     accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
     secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
 }
 
-router.post('/', (req,res) => {
+
+router.post('/s3', (req,res) => {
     const file = req.body;
     console.log('in router post', file);
     uploadFile(file, config)
@@ -23,4 +22,6 @@ router.post('/', (req,res) => {
             res.send(data);
         })
         .catch(err => console.error(err))
-}
+})
+
+module.exports = router;
