9db5433a105a34e6a2c3ac529daa0803565f47f9
Sarah Fuoss <sfuoss@gmail.com>
2021-10-12 20:01:44 -0500

    Added a dispatch action on page load for LogHistory component and accessed the stored information from the reducer.

--- src/components/LogHistory/LogHistory.jsx
+++ src/components/LogHistory/LogHistory.jsx
@@ -1,6 +1,21 @@
+import { useSelector } from 'react-redux';
+
 function LogHistory() {
+
+    // access the log history
+    // from the logHistory reducer
+    const store = useSelector(store => store.logHistory)
+    
+
+    // calling saga function on page load
+    // to get the log history from the server
+    useEffect(() => {
+        dispatch({ type: 'FETCH_LOG_HISTORY' });
+      }, [dispatch]);
+    
     return (
         <>
+        {JSON.stringify(store)}
         </>
     );
 }
