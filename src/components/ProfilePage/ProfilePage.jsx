0d07f94aa47065325c98293a7cc2e2e1c96b32d5
Sarah Fuoss <sfuoss@gmail.com>
2021-10-14 10:58:28 -0500

    Cleaned up code and added comments.

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { readAndCompressImage } from 'browser-image-resizer';
-import { useParams } from 'react-router-dom';
-
 
 
 function ProfilePage() {
@@ -20,16 +18,11 @@ function ProfilePage() {
     // variables to access and hold data
     // from the root reducer store
     const profileInfo = useSelector(store => store.profile.profileInfoReducer);
-    const profilePicUrl = useSelector(store => store.profile.profilePictureReducer);
     const userInfo = useSelector(store => store.user)
 
     // getting user id from the store to send 
     // with uploaded photo
     const userId = userInfo.id;
-
-    const allParams = useParams();
-    const profileUserName = allParams.username;
-    const profilePicture = allParams.profile_picture_medium;
 
     // hooks for image actions
     const [preview, setPreview] = useState('');
@@ -39,7 +32,6 @@ function ProfilePage() {
     // asynchronous function that
     // updates hooks from user inputted information
     const onFileChange = async (event) => {
-        console.log('profileusername', profileUserName);
         console.log(event);
         const userFile = event.target.files[0];
         // const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/jpg'];
@@ -70,16 +62,8 @@ function ProfilePage() {
             }
         };
         dispatch(action);
+        setPreview('');
     }
-
-    // const postPhoto = () => {
-    //     let action;
-    //     action = {
-    //         type: 'POST_PHOTO',
-    //         payload: selectedFile,
-    //     }
-    //     dispatch(action);
-    // }
 
     // gets the user information from the reducer
     // on page load
@@ -89,6 +73,8 @@ function ProfilePage() {
 
     return (
         <>
+        {/* display preview of image once selected
+        onFileChange sets the state of preview */}
             {preview && (
                 <img
                     className="placeholder-photo-preview"
@@ -96,26 +82,19 @@ function ProfilePage() {
                     alt="Photo preview"
                 />
             )}
+            {/* Allows user to select a file from their local files */}
             <input type="file" accept="image/*" onChange={onFileChange} />
+            {/* Dispatches file to saga when the button is clicked */}
             <button onClick={event => sendFormDataToServer()}>Submit</button>
-            <p>Profile img url</p>
-             <p>{JSON.stringify(profilePicUrl)}</p><hr />
-             <p>profile info</p>
-            {JSON.stringify(profileInfo)} <br /> <hr />
-            <p>user params</p>
-            <p>
-            {JSON.stringify(profileUserName)}
-            </p>
-            {/* <img src= {profilePicUrl[0].profile_picture_medium}></img> */}
-            {/* <h1>{profileUserName}</h1> */}
+            {/* <p>{JSON.stringify(profileUserName)}</p> */}
+            {/* Map over the profileInfoReducer 
+            to display username and profile image */}
             {profileInfo.map((user) =>
                     <div key={user.id}>
                         <p>username: {user.username}</p>
                         <img src= {user.profile_picture_medium}></img>
                     </div>
                 )}
-                <img src={profilePicUrl.profile_picture_thumb} />
-                {/* <img src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"></img> */}
         </>
 
     );
