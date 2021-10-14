d08bf3455d0db90a00a3ec1d7d8c59af7d2ffda8
Sarah Fuoss <sfuoss@gmail.com>
2021-10-13 21:37:51 -0500

    Added code comments.

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -12,15 +12,28 @@ function ProfilePage() {
         maxHeight: 300,
     };
 
+    // variable to access useDispatch from
+    // react redux module for use in sending
+    // information to the saga
+    const dispatch = useDispatch();
+
+    // variables to access and hold data
+    // from the root reducer store
     const profileInfo = useSelector(store => store.profile.profileInfoReducer);
     const profilePicUrl = useSelector(store => store.profile.profilePictureReducer);
     const userInfo = useSelector(store => store.user)
+
+    // getting user id from the store to send 
+    // with uploaded photo
     const userId = userInfo.id;
+
+    // hooks for image actions
     const [preview, setPreview] = useState('');
     const [selectedFile, setSelectedFile] = useState('');
-    const dispatch = useDispatch();
     const [resizedFile, setResizedFile] = useState('');
 
+    // asynchronous function that
+    // updates hooks from user inputted information
     const onFileChange = async (event) => {
         console.log(event);
         const userFile = event.target.files[0];
@@ -37,6 +50,7 @@ function ProfilePage() {
         // }
     }
 
+    // send information from hooks to saga function
     const sendFormDataToServer = () => {
         let action;
         // The file name seems to be dropped on resize, send both the
@@ -53,9 +67,11 @@ function ProfilePage() {
         dispatch(action);
     }
 
+    // gets the user information from the reducer
+    // on page load
     useEffect(() => {
         dispatch({ type: 'FETCH_PROFILE_INFO' });
-      }, [dispatch]);
+      }, []);
 
     return (
         <>
@@ -68,8 +84,12 @@ function ProfilePage() {
             )}
             <input type="file" accept="image/*" onChange={onFileChange} />
             <button onClick={event => sendFormDataToServer()}>Submit</button>
-            {JSON.stringify(profilePicUrl)}
-            {JSON.stringify(profileInfo)}
+            <p>Profile info</p>
+             <p>{JSON.stringify(profilePicUrl)}</p><hr />
+            {JSON.stringify(profileInfo)} <br /> <hr />
+            {/* <img src= {profilePicUrl[0].profile_picture_medium}></img> */}
+            {/* <h1>{profileInfo}</h1> */}
+
         </>
 
     );
