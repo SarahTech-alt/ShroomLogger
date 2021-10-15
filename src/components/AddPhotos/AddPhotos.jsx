0db1d017eb2ab6d8d1892922af6072a16e619e50
Sarah Fuoss <sfuoss@gmail.com>
2021-10-15 15:13:04 -0500

    Created dispatch event for inputs.

--- src/components/AddPhotos/AddPhotos.jsx
+++ src/components/AddPhotos/AddPhotos.jsx
@@ -1,19 +1,25 @@
 import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function AddPhotos() {
     // use history for user navigation between pages
-    const history = useHistory()
-
+    const history = useHistory();
+    // use to dispatch events to sagas
+    const dispatch = useDispatch();
+    // hook for storing input data
     const [newMushroom, setNewMushroom] = useState({
         commonName: '',
         scientificName: '',
         location: '',
         date: '',
         details: '',
-    })
-
-        
+    });
+        // dispatch newMushroom info from inputs to mushroom.saga
+        const addNewMushroom = () => {
+            dispatch({ type: 'ADD_NEW_MUSHROOM', payload: newMushroom})
+            history.goBack();
+        }
 
 
     return(
