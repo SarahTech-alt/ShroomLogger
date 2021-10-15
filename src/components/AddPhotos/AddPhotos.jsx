65990d8443f3e753b00945e35a6a555aba82bad9
Sarah Fuoss <sfuoss@gmail.com>
2021-10-15 14:24:39 -0500

    Created user inputs for AddPhotos

--- src/components/AddPhotos/AddPhotos.jsx
+++ src/components/AddPhotos/AddPhotos.jsx
@@ -6,7 +6,14 @@ function AddPhotos() {
 
     return(
         <>
+        <p>picture upload placeholder</p>
+        <input type="text" placeholder="Common Name"></input> <br />
+        <input type="text" placeholder="Scientific Name (optional)"></input><br />
+        <input type="text" placeholder="Where"></input> <br />
+        <input type="text" placeholder="When"></input> <br />
+        <input type="text" placeholder="Additional Details"></input> <br />
         <button onClick={event => history.goBack()}>Go Back</button>
+        <button>Add</button>
         </>
     );
 }
