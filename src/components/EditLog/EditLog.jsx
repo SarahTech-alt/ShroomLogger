922a1c50bada1facdad5187d27abcb4df26ecb0b
Sarah Fuoss <sfuoss@gmail.com>
2021-10-14 19:44:35 -0500

    Filled out EditLog page with mapped data for selected log.

--- src/components/EditLog/EditLog.jsx
+++ src/components/EditLog/EditLog.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function EditLog() {
     const allParams = useParams();
-    const logDetails = useSelector(store => store.logHistory.logDetail);
+    const logDetails = useSelector(store => store.logHistory);
     const logId = allParams.id;
     const dispatch = useDispatch();
 
@@ -14,8 +14,22 @@ function EditLog() {
 
     return (
         <>
+        {/* {JSON.stringify(logDetails.logHistory)} */}
         <h1>Edit Page</h1>
+        <table>
+                <tbody>
+                {logDetails.logHistory.map((logs) => (
+                    <tr key={logs.log_id}><br />
+                    <tr><button>edit</button> Common Name: {logs.common_name}</tr><br />
+                    <tr><button>edit</button> Scientific Name: {logs.scientific_name}</tr><br />
+                    <tr><button>edit</button> Date of Entry: {logs.date} </tr><br />
+                    <tr><button>edit</button> Description: {logs.details} </tr>
+                    </tr>
+                ))}
+               </tbody>
+            </table>
         </>
+        
     );
 }
 
