0f7c3c0bc4087feb986b90d5962019ba6ea72666
Sarah Fuoss <sfuoss@gmail.com>
2021-10-14 20:01:31 -0500

    Converted table to div and paragrphs.

--- src/components/EditLog/EditLog.jsx
+++ src/components/EditLog/EditLog.jsx
@@ -16,18 +16,15 @@ function EditLog() {
         <>
         {/* {JSON.stringify(logDetails.logHistory)} */}
         <h1>Edit Page</h1>
-        <table>
-                <tbody>
                 {logDetails.logHistory.map((logs) => (
-                    <tr key={logs.log_id}><br />
-                    <tr><button>edit</button> Common Name: {logs.common_name}</tr><br />
-                    <tr><button>edit</button> Scientific Name: {logs.scientific_name}</tr><br />
-                    <tr><button>edit</button> Date of Entry: {logs.date} </tr><br />
-                    <tr><button>edit</button> Description: {logs.details} </tr>
-                    </tr>
+                    <div key={logs.log_id}>
+                    <p><button>edit</button> Common Name: {logs.common_name}</p>
+                    <p><button>edit</button> Scientific Name: {logs.scientific_name}</p>
+                    <p><button>edit</button> Date of Entry: {logs.date} </p>
+                    <p><button>edit</button> Description: {logs.details} </p>
+                    </div>
                 ))}
-               </tbody>
-            </table>
+            
         </>
         
     );
