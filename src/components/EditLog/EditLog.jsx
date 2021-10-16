17268fac9e29c9cea6a7052d94a5603da29fb0d2
Sarah Fuoss <sfuoss@gmail.com>
2021-10-16 18:32:10 -0500

    Created text input fields for values to edit.

--- src/components/EditLog/EditLog.jsx
+++ src/components/EditLog/EditLog.jsx
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import './EditLog.css';
 
 function EditLog() {
     // matches parameters of current route
@@ -26,21 +27,46 @@ function EditLog() {
     const deleteLog = () => {
         dispatch({ type: 'DELETE_SELECTED_LOG', payload: logId })
     }
+    const [newMushroom, setNewMushroom] = useState({
+        common_name: '',
+        scientific_name: '',
+        latitude: '',
+        longitude: '',
+        date: '',
+        details: '',
+        mushroom_picture_url: '',
+
+    });
+
+
+
+    const [editThing, setEditThing] = useState(false)
+
 
     return (
         <>
-            {JSON.stringify(logInfo.logDetail)}
+            {JSON.stringify(logInfo.logDetail)}<hr/>
             {/* Access information from the logDetail
             reducer and display on DOM with buttons to edit logs
             and a back button to navigate to previous page */}
             <h1>Edit Page</h1>
-            <button onClick={event => deleteLog()}>delete log</button>
-            <p><button>edit</button> Common Name: {selectedLog.common_name}</p>
-            <p><button>edit</button> Scientific Name: {selectedLog.scientific_name}</p>
-            <p><button>edit</button> Date of Entry: {selectedLog.date} </p>
-            <p><button>edit</button> Description: {selectedLog.details} </p>
-            <img src={selectedLog.mushroom_picture_url} alt={selectedLog.mushroom_picture_url}></img><br />
+            <button onClick={event => deleteLog()}>delete log</button><br /><br />
+            <input type="text" placeholder={selectedLog.common_name}/><br />
+            <input type="text" placeholder={selectedLog.scientific_name}/><br />
+            <input type="text" placeholder={selectedLog.details}/><br/>
+            <input type="date" /><br/>
+            <img src={selectedLog.mushroom_picture_url} alt={selectedLog.mushroom_picture_url}></img><br /> <br/>
+            {editThing && (
+                <div>
+                    <input type="text" />
+                    {/* Dispatches file to saga when the button is clicked */}
+                    <button onClick={event => sendFormDataToServer()}>Submit</button>
+                </div>
+            )}
+
+            {/* <button onClick={event => setEditThing(!editThing)}>Edit</button> */}
             <button onClick={event => history.goBack()}>back</button>
+            
         </>
     );
 }
